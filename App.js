af67beb93383a77db7ec888da35ef6de11d8c14a
Uriel Zacarias <urielzacarias@gmail.com>
2022-03-05 01:52:34 -0800

    added button that changes text displayed

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start writing on your app!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
+      <Button 
+        title="Change Text" 
+        onPress={()=> setOutputText('The text has been updated!')}
+      />
       <StatusBar style="auto" />
     </View>
   );
